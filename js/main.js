54af062699ae813d9cc199d875d1f990fb85d9fe
jbkkd <omerkorner@gmail.com>
2013-10-26 18:00:28 +0200

    Version 0.5 Added support for Departure or Arrival

--- js/main.js
+++ js/main.js
@@ -49,13 +49,18 @@ function initialize() {
 function calcRoute() {
     var start = document.getElementById('start').value;
     var end = document.getElementById('end').value;
-    var date = $("#time").datetimepicker("getDate");
+    var departureTime = $("#time").datetimepicker("getDate");
+    var arrivalTime;
+    if ($("#departureArrival option:selected").val() == "arrive") {
+        arrivalTime = $("#time").datetimepicker("getDate");
+    }
     var request = {
         origin: start,
         destination: end,
         travelMode: google.maps.TravelMode.TRANSIT,
         transitOptions: {
-            departureTime: date
+            departureTime: departureTime,
+            arrivalTime: arrivalTime
         }
     };
     directionsService.route(request, function(response, status) {
@@ -71,19 +76,22 @@ function ReplaceBahnlinkVariables(bahnlink, startFullAddress, endFullAddress) {
     var date = new $("#time").datetimepicker("getDate");
     bahnlink = bahnlink.replace("STARTDESTINATION", startFullAddress);
     bahnlink = bahnlink.replace("ENDDESTINATION", endFullAddress);
-    bahnlink = bahnlink.replace("DATEOFDEPARTURE", date.toLocaleDateString("de-DE"));
-    bahnlink = bahnlink.replace("TIMEOFDEPARTURE", date.getHours() + ":" + date.getMinutes());
+    bahnlink = bahnlink.replace("DATEOFJOURNEY", date.toLocaleDateString("de-DE"));
+    bahnlink = bahnlink.replace("TIMEOFJOURNEY", date.getHours() + ":" + date.getMinutes());
+    bahnlink = bahnlink.replace("DEPARTORARRIVE", $("#departureArrival option:selected").val());
     return bahnlink;
 }
+
 function addBuyNowLink() {
-    var bahnlink = "http://reiseauskunft.bahn.de/bin/query.exe/en?existOptimizePrice=1&&S=STARTDESTINATION&REQ0JourneyStopsSID=&REQ0JourneyStopsS0A=7&ignoreTypeCheck=no&Z=ENDDESTINATION&REQ0JourneyStopsZ0A=7&trip-type=single&date=Tu%2C+DATEOFDEPARTURE&time=TIMEOFDEPARTURE&timesel=depart&returnTimesel=depart&optimize=0&travelProfile=-1&adult-number=1&children-number=0&infant-number=0&tariffTravellerType.1=E&tariffTravellerReductionClass.1=0&tariffTravellerAge.1=&qf-trav-bday-1=&tariffTravellerReductionClass.2=0&tariffTravellerReductionClass.3=0&tariffTravellerReductionClass.4=0&tariffTravellerReductionClass.5=0&tariffClass=2&start=1";
+    var bahnlink = "http://reiseauskunft.bahn.de/bin/query.exe/en?existOptimizePrice=1&&S=STARTDESTINATION&REQ0JourneyStopsSID=&REQ0JourneyStopsS0A=7&ignoreTypeCheck=no&Z=ENDDESTINATION&REQ0JourneyStopsZ0A=7&trip-type=single&date=Tu%2C+DATEOFJOURNEY&time=TIMEOFJOURNEY&timesel=DEPARTORARRIVE&returnTimesel=depart&optimize=0&travelProfile=-1&adult-number=1&children-number=0&infant-number=0&tariffTravellerType.1=E&tariffTravellerReductionClass.1=0&tariffTravellerAge.1=&qf-trav-bday-1=&tariffTravellerReductionClass.2=0&tariffTravellerReductionClass.3=0&tariffTravellerReductionClass.4=0&tariffTravellerReductionClass.5=0&tariffClass=2&start=1";
     var startPlace = autoCompleteStart.getPlace();
     var startStreetNumber, startStreet, startCity, startFullAddress;
     var endPlace = autoCompleteEnd.getPlace();
     var endStreetNumber, endStreet, endCity, endFullAddress;
-    for (var i = 0; i < startPlace.address_components.length; i++) {
-        var addressType = startPlace.address_components[i].types[0];
-        var nameOfAddress = startPlace.address_components[i].long_name;
+    var i, addressType, nameOfAddress;
+    for (i = 0; i < startPlace.address_components.length; i++) {
+        addressType = startPlace.address_components[i].types[0];
+        nameOfAddress = startPlace.address_components[i].long_name;
         if (addressType == "locality") {
             startCity = nameOfAddress
         }
@@ -96,9 +104,9 @@ function addBuyNowLink() {
             startStreet = nameOfAddress;
         }
     }
-    for (var i = 0; i < endPlace.address_components.length; i++) {
-        var addressType = endPlace.address_components[i].types[0];
-        var nameOfAddress = endPlace.address_components[i].long_name;
+    for (i = 0; i < endPlace.address_components.length; i++) {
+        addressType = endPlace.address_components[i].types[0];
+        nameOfAddress = endPlace.address_components[i].long_name;
         if (addressType == "locality") {
             endCity = nameOfAddress;
         }
